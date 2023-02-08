f6b60b8fc9f5bcfdfc142d592725e168b37d969e
@Ragaja_K <35251364+ragaja-14@users.noreply.github.com>
2023-02-08 08:22:48 +0530

    removed config values

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 
-config= {"google_api_key":"AIzaSyDgcedjgZWIIxNZEYKenO5BXHOt448gJSs",
-        "playlist_ID":"OLAK5uy_lwgBrqdaPtgrMvKfMJuMVRT8JRAIb84Xo",
+config= {"google_api_key":"<Google API Key>",
+        "playlist_ID":"<playlist ID to track video stats>",
         "kafka": {
-            "bootstrap.servers": "pkc-xrnwx.asia-south2.gcp.confluent.cloud:9092",
+            "bootstrap.servers": "<host-endpoint>",
             "security.protocol": "sasl_ssl",
             "sasl.mechanism": "PLAIN",
-            "sasl.username": "GBQRRLGW7QC3PJAN",
-            "sasl.password": "AJjFxiXmualQidNcQXKd9Ao98TkEyu13BWtePXzTRJMLrTKnJERgaDzZy51jMMLh",
+            "sasl.username": "<username for cluster>",
+            "sasl.password": "<password>",
             },
         "schema_registry": {
-            "url": "https://psrc-y5q2k.europe-west3.gcp.confluent.cloud:443",
-            "basic.auth.user.info": "V5AR3DPIJT4QB6FY:WL+3N669FXxKZHYTwBzR+BGk90x1CnXDa9AGoYXStYylmCjxFfa9zNpQxJS926mC",
+            "url": "<Schema registry endpoint >",
+            "basic.auth.user.info": "<API key as username>:<API secret as password>",
             }
 
         }
